0092bbdd92abceeda4dec979391c72d94d10c73f
Ayeeshi Poosarla <Ayeeshi@macintoshusers-macbook-2.local>
2018-12-01 16:34:23 -0800

    Initial Commit

---
+++ PocketMoney/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PocketMoney
+//
+//  Created by Ayeeshi Poosarla on 12/1/18.
+//  Copyright © 2018 Ayeeshi Poosarla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
