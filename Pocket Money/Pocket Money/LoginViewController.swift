caaebf9527d77c8e9999d49ac5e3bbb5b488936c
Ayeeshi Poosarla <Ayeeshi@macintoshusers-macbook-2.local>
2018-12-21 18:48:45 -0800

    Add Buttons and Labels to Login, SignUp, and Tasker SignUp

---
+++ Pocket Money/Pocket Money/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  Pocket Money
+//
+//  Created by Ayeeshi Poosarla on 12/21/18.
+//  Copyright © 2018 Ayeeshi Poosarla. All rights reserved.
+//
+
+import UIKit
+
+
+var username = "username"
+var password = "password"
+
+class LoginViewController: UIViewController {
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pocket Money/Pocket Money/SignupTaskerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignupTaskerViewController.swift
+//  Pocket Money
+//
+//  Created by Ayeeshi Poosarla on 12/21/18.
+//  Copyright © 2018 Ayeeshi Poosarla. All rights reserved.
+//
+
+import UIKit
+
+class SignupTaskerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
