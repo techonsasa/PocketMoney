96ab4c1de3d1ae3072011a682a3f8d6c4a80481f
Sharra Kutty <sharra.v.kutty@gmail.com>
2019-01-25 19:27:17 -0800

    7:27 PM Sharra

---
+++ Pocket Money/Pocket Money/TaskeeHomeScreenVC.swift
@@ -0,0 +1,40 @@
+//
+//  TaskeeHomeScreenVC.swift
+//  Pocket Money
+//
+//  Created by Sharra Kutty on 1/25/19.
+//  Copyright © 2019 Ayeeshi Poosarla. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TaskeeHomeScreenVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let job = {}
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    
+    
+    
+}
