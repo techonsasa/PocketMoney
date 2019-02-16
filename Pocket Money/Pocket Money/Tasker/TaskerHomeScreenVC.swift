6e69baf83660088c9729d7d39033c11df80420d3
Ayeeshi Poosarla <ayeeshi.poosarla@gmail.com>
2019-02-15 18:47:54 -0800

    647 Ayeeshi

--- Pocket Money/Pocket Money/Tasker/TaskerHomeScreenVC.swift
+++ Pocket Money/Pocket Money/Tasker/TaskerHomeScreenVC.swift
@@ -8,32 +8,32 @@
 
 import UIKit
 
-class TaskerHomeScreenVC : UIViewController, UITableViewDelegate, UITableViewDataSource{
+//class TaskerHomeScreenVC : UIViewController, UITableViewDelegate, UITableViewDataSource{
 
-    
-    var names = ["lorman", "sharra", "bill", "bob"]
-    
-    var taskTime = ["11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM"]
+//
+//    var names = ["lorman", "sharra", "bill", "bob"]
+//
+//    var taskTime = ["11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM"]
+//
+//
+//    @IBOutlet weak var jobPostingTableView2: UITableView!
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return names.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let jobCell2 = tableView.dequeueReusableCell(withIdentifier: "jobPostingCell2", for: indexPath) as! JobPostingCell2
+//        jobCell2.nameOfTask2.text = names[indexPath.row]
+//        jobCell2.timeOfTask2.text = taskTime[indexPath.row]
+//        return jobCell2
+//
+//    }
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(names[indexPath.row])
+//    }
+
   
-    
-    @IBOutlet weak var jobPostingTableView2: UITableView!
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let jobCell2 = tableView.dequeueReusableCell(withIdentifier: "jobPostingCell2", for: indexPath) as! JobPostingCell2
-        jobCell2.nameOfTask2.text = names[indexPath.row]
-        jobCell2.timeOfTask2.text = taskTime[indexPath.row]
-        return jobCell2
-        
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(names[indexPath.row])
-    }
 
-  
-
-}
+//}
