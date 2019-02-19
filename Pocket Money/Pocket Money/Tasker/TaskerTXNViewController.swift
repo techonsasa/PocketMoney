c4db03267f88af07b9b414a23ee6ae3d66808b03
Sathiyan Kutty <sathiyan.k@gmail.com>
2019-02-18 22:40:11 -0800

    Seth 2/18 7:17 PM

--- Pocket Money/Pocket Money/Tasker/TaskerTXNViewController.swift
+++ Pocket Money/Pocket Money/Tasker/TaskerTXNViewController.swift
@@ -1,18 +1,20 @@
 //
-//  TaskeeTXNViewController.swift
-//  
+//  TaskerTXNViewController.swift
+//  Pocket Money
 //
-//  Created by Sharra Kutty on 2/18/19.
+//  Created by Sathiyan Kutty on 2/18/19.
+//  Copyright © 2019 Ayeeshi Poosarla. All rights reserved.
 //
 
 import UIKit
 
-class TaskeeTXNViewController: UIViewController {
+class TaskerTXNViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView!
-
+    @IBOutlet weak var tableView1: UITableView!
+    
 //    var tasks = ["Patience Stromberg", "Vonnie Heintzelman","Audria Yount","David Mcneely","Teisha Kanne","Jay Mccloy","Mark Russo","Carmelita Focht"]
 //    var amounts = ["$20","$30","$100","$50","$20","$30","$10","$40"]
+//    var apps = TIBApps.getAllApps()
     
     var txns = [(tasker: "Patience Stromberg", amount:"$20"),
                 (tasker: "Vonnie Heintzelman", amount:"$30"),
@@ -26,37 +28,43 @@ class TaskeeTXNViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        tableView.dataSource = self
+        
+       tableView1.dataSource = self
     }
 
-  
 }
 
-extension TaskeeTXNViewController: UITableViewDataSource
+extension TaskerTXNViewController: UITableViewDataSource
 {
-    func numberOfSections(in tableView: UITableView) -> Int {
+    
+    func numberOfSections(in tableView1: UITableView) -> Int {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       // return tasks.count
+      //  return apps.count
         return txns.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "TaskeeTXNCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskerTXNCell")!
 
         let app = txns[indexPath.row]
         cell.textLabel?.text=app.tasker
         cell.detailTextLabel?.text=app.amount
 
-//    let app = tasks[indexPath.row]
-//     let amt = amounts[indexPath.row]
-//     print (app)
-//     cell.textLabel?.text=app
-//     cell.detailTextLabel?.text=amt
+        
+//        let app = tasks[indexPath.row]
+//        let amt = amounts[indexPath.row]
+//        cell.textLabel?.text=app
+//        cell.detailTextLabel?.text=amt
 
-    return cell
-    
+//        let app=apps[indexPath.row]
+//        print (app)
+//        cell.textLabel?.text=app.name
+
+        return cell
+
     }
 }
