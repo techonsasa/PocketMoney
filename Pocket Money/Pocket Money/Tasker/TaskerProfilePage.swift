a60fae6e824a996b68ccb9b2e4cbecd56edf2c3e
Avishi Goyal <avishigoyal@Mohans-MacBook-Pro.local>
2019-02-23 21:57:50 -0800

    Avishi 9:57

---
+++ Pocket Money/Pocket Money/Tasker/TaskerProfilePage.swift
@@ -0,0 +1,35 @@
+//
+//  TaskerProfilePage.swift
+//  Pocket Money
+//
+//  Created by Avishi Goyal on 2/23/19.
+//  Copyright © 2019 Ayeeshi Poosarla. All rights reserved.
+//
+
+import UIKit
+
+class TaskerEditVC : UIViewController, UINavigationControllerDelegate {
+    var imagePicker: UIImagePickerController?
+    @IBOutlet var preveiwPFP: UIImageView!
+    
+    @IBAction func openCamera(_ sender: Any) {
+        if (UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            imagePicker = UIImagePickerController()
+            imagePicker?.delegate = self
+            imagePicker?.sourceType = .camera
+            imagePicker?.allowsEditing = false
+            self.present(imagePicker!, animated: true, completion: nil)
+        }
+    }
+}
+
+extension TaskeeEditVC: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        preveiwPFP.image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
