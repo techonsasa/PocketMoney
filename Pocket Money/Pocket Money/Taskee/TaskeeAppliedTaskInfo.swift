ce3d2ba5cf394b65c9c2e51cda4e168b8f714a96
Ayeeshi Poosarla <ayeeshi.poosarla@gmail.com>
2019-04-13 09:36:39 -0700

    936 Ayeeshi

--- Pocket Money/Pocket Money/Taskee/TaskeeAppliedTaskInfo.swift
+++ Pocket Money/Pocket Money/Taskee/TaskeeAppliedTaskInfo.swift
@@ -18,10 +18,19 @@ class TaskeeAppliedTaskInfo: UIViewController {
     @IBOutlet var taskWage: UILabel!
     @IBOutlet var taskTime: UILabel!
     @IBOutlet var TaskDescription: UITextView!
-    
     @IBOutlet var taskDate: UILabel!
     
     override func viewDidLoad() {
-        print(taskData)
+        taskName.text = taskData!["jobName"] as! String
+        taskerName.text = taskData!["taskerName"] as! String
+        taskWage.text = taskData!["taskRate"] as! String
+        taskTime.text = taskData!["jobTime"] as! String
+        TaskDescription.text = taskData!["jobDescription"] as! String
+        taskDate.text = taskData!["taskeDate"] as! String
     }
+    
+    @IBAction func okayButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
